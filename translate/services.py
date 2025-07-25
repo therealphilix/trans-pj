9d1f17d36babb3fe70e9a0ebea6598501db6a9e2
therealphilix <uchennafelix888@gmail.com>
2025-07-25 16:27:55 +0100

    second commit

--- translate/services.py
+++ translate/services.py
@@ -1,16 +1,38 @@
 from google.cloud import translate_v2 as translate
 from google.cloud.exceptions import GoogleCloudError
 from django.conf import settings
+from google.oauth2 import service_account
 import logging
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 logger = logging.getLogger(__name__)
 
+# Create credentials from environment variables
+def get_google_credentials():
+    credentials_dict = {
+        "type": "service_account",
+        "project_id": os.getenv('PROJECT_ID'),
+        "private_key_id": os.getenv('PRIVATE_KEY_ID'),
+        "private_key": os.getenv('PRIVATE_KEY').replace('\\n', '\n'),
+        "client_email": os.getenv('CLIENT_EMAIL'),
+        "client_id": os.getenv('CLIENT_ID'),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": f"https://www.googleapis.com/robot/v1/metadata/x509/{os.getenv('CLIENT_EMAIL')}",
+        "universe_domain": "googleapis.com"
+    }
+    
+    return service_account.Credentials.from_service_account_info(credentials_dict)
+
 class GoogleCloudTranslationService:
     def __init__(self):
         try:
-            self.translate_client = translate.Client()
+            # Get credentials and pass them to the client
+            credentials = get_google_credentials()
+            self.translate_client = translate.Client(credentials=credentials)
         except Exception as e:
             logger.error(f"Failed to initialize Google Cloud Translation client: {str(e)}")
             raise
